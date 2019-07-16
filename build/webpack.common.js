02637198e243d87734f0f6371fed79d44129e332
guo <503392549@qq.com>
2019-07-16 08:38:26 +0800

    end spliting

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -59,27 +59,8 @@ module.exports={
     // 代码分割
     optimization:{
         splitChunks: {
-            chunks: 'all', // async ,all 
-            minSize: 30000,
-            maxSize: 0, // 对大于maxsize的文件尝试进行二次拆分
-            minChunks: 1, // 被引用超过minChunk次才进行代码分割
-            maxAsyncRequests: 5,// 最大同时加载数，分割超过5个，后面的不再加载
-            maxInitialRequests: 3, 
-            automaticNameDelimiter: '~',
-            automaticNameMaxLength: 30,
-            name: true,
-            cacheGroups: { //缓存组
-              vendors: {
-                test: /[\\/]node_modules[\\/]/, //引入的库若在此目录下，则进行分割
-                priority: -10,
-                // filename:'venders.js' // 打包到一起的自定义文件名
-              },
-              default: {
-                priority: -20,
-                filename:'common.js',
-                reuseExistingChunk: true,//不会重复引用
-              }
-            }
+            chunks: 'all', 
+           
           }
     },
     plugins: [
